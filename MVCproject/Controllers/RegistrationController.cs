e9f4ccd03c225123cae9712b68c4715de71cf819
muhammad.fazal <fazal.abbas4@gmail.com>
2020-11-25 17:04:54 +0500

    set login

---
+++ MVCproject/Controllers/RegistrationController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCproject.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        // GET: Registration
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
