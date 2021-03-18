c3d63837ee1a9bbaa23b173bad36b36a33378c12
Muhammad Fazal <fazal_tech@outlook.com>
2021-03-18 12:00:18 +0500

    add receive controller

---
+++ MVCproject/Controllers/Receive_ProductController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCproject.Controllers
+{
+    public class Receive_ProductController : Controller
+    {
+        // GET: Receive_Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
